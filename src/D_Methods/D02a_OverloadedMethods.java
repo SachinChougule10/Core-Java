04655b389d90dc03049ae3c2162cdf4df0c4750b
Sachin Chougule <sachinchougule5762@gmail.com>
2025-12-18 23:06:46 +0530

    feat: Add Java examples demonstrating method overloading

---
+++ src/D_Methods/D02a_OverloadedMethods.java
@@ -0,0 +1,52 @@
+/*
+ * This program demonstrates method overloading in Java.
+ *
+ * It shows:
+ *  - How multiple methods can share the same name but have different parameter lists
+ *  - How Java selects the correct method based on the method signature
+ *  - Examples of overloaded 'add' methods with different numbers of parameters
+ *
+ * Key Concepts:
+ *  - Method Overloading: Same method name, different parameters
+ *  - Method Signature: Method name + parameter types (NOT the return type)
+ */
+
+package D_Methods;
+
+public class D02a_OverloadedMethods {
+    public static void main(String[] args){
+
+        // overloaded methods :- methods that share the same name, but different parameters
+        // method signature = method's name + parameters
+
+        System.out.println(add(2,3,5,6));
+
+    }
+
+    static double add(double a, double b){
+        return a + b;
+    }
+
+    static double add(double a, double b, double c){
+        return a + b + c;
+    }
+
+    // when we try to pass 3 arguments in the up given method, which accepts only 2 arguments, we will get an error that 'Expected 2 arguments but found 3'.
+    // solution for this is that we can create a method with same name 'add', having different parameters
+
+
+    static double add(double a, double b, double c, double d){
+        return a + b + c + d;
+    }
+}
+
+
+/*
+
+1) Two D_Methods.methods can have same name and different parameters,
+   but two D_Methods.methods cannot have same name and same parameters.
+-  Java gives an error of method is already defined in the specific file name.
+
+2) No two D_Methods.methods can have same method signature
+
+ */
---
+++ src/D_Methods/D02b_OverloadedMethodsExample1.java
@@ -0,0 +1,38 @@
+/*
+ * This program demonstrates method overloading using a real-world example: baking pizza.
+ *
+ * It shows:
+ *  - How multiple methods can share the same name but differ in their parameters.
+ *  - How Java decides which overloaded method to call based on the number and type of arguments.
+ *
+ * Key Concept:
+ *  - Compile-time polymorphism (method overloading)
+ */
+
+package D_Methods;
+
+public class D02b_OverloadedMethodsExample1 {
+    public static void main(String[] args){
+
+        System.out.println(bakePizza("flatbread","mozerella","pepperoni"));
+    }
+
+    static String bakePizza(String bread){
+        return bread + " " + "pizza";
+    }
+
+    static String bakePizza(String bread, String cheese){
+        return cheese + " " + bread + " " + "pizza";
+    }
+
+    static String bakePizza(String bread, String cheese, String toppings){
+        return bread  + " " + cheese + " " + toppings + " " + "pizza";
+    }
+
+
+}
+
+// This is a form of compile-time polymorphism.
+// if there are two are more D_Methods.methods with same name, we will end up using the method which match the number of parameters
+
+// “The method that has the same number and type of parameters as the arguments passed in the method call will be used.”
